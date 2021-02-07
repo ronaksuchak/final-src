974693036ecd244b0da7cc7df0fa2ea380de6d10
Ronak Suchak <ronaksuchak16@hotmail.com>
2021-02-07 20:28:48 +0530

    docker file created

--- OCMS-Front-End/src/app/app.component.ts
+++ OCMS-Front-End/src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {CommonModule} from '@angular/common';
-import{ReactiveFormsModule,FormsModule,FormControl,FormGroup} from '@angular/forms';
-import {MatFormFieldControl,MatFormField,MatFormFieldModule} from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule, FormControl, FormGroup } from '@angular/forms';
+import { MatFormFieldControl, MatFormField, MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +10,10 @@ import {MatFormFieldControl,MatFormField,MatFormFieldModule} from '@angular/mate
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'OCMS-Front-End';
+  title = 'frontend';
 
-  constructor(private router: Router){}
+  constructor(private router: Router) { }
 
-  ngOnInit(){
+  ngOnInit() {
   }
 }
